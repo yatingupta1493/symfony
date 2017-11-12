8d2ff48f464d19a88d88e25c2e3715dbf7875f06
yatin1493 <yatin.gupta1493@gmail.com>
2017-11-12 04:29:31 -0800

    added

--- src/AppBundle/Controller/TodoController.php
+++ src/AppBundle/Controller/TodoController.php
@@ -2,9 +2,15 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Todos;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class TodoController extends Controller
 {
@@ -14,7 +20,13 @@ class TodoController extends Controller
     public function indexAction()
     {
         //die("something great");
-        return $this->render('todo/index.html.twig');
+        $todosList = $this->getDoctrine()
+                          ->getRepository("AppBundle:Todos")
+                          ->findAll();
+
+        return $this->render('todo/index.html.twig', array(
+          "todosList" => $todosList
+        ));
     }
     /**
      * @Route("/todos/create", name="todo_create")
@@ -22,7 +34,44 @@ class TodoController extends Controller
     public function createAction(Request $request)
     {
         //die("something great");
-        return $this->render('todo/create.html.twig');
+        $todos = new Todos();
+        $form = $this->createFormBuilder($todos)
+                     ->add('name', TextType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
+                     ->add('description', TextareaType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
+                     ->add('priority', ChoiceType::class, array('choices' => array('Low' => "low", 'Medium' => "medium", 'High' => "high"), 'attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
+                     ->add('due_date', DateTimeType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
+                     ->add('save', SubmitType::class, array('label' => 'Save', 'attr' => array('class' => 'btn btn-primary', 'style' => 'margin-bottom:15px')))
+                     ->getForm();
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+          //Get Data
+          $name = $form['name']->getData();
+          $description = $form['description']->getData();
+          $priority = $form['priority']->getData();
+          $due_date = $form['due_date']->getData();
+
+          $now = new\DateTime('now');
+          $todos->setName($name);
+          $todos->setDescription($description);
+          $todos->setPriority($priority);
+          $todos->setDueDate($due_date);
+          $todos->setCreateDate($now);
+
+          $em = $this->getDoctrine()->getManager();
+          $em->persist($todos);
+          $em->flush();
+
+          $this->addFlash('notice', 'New Todo task is added');
+          $this->redirectToRoute('todo_list');
+        }
+        else {
+          echo "doing nothing!!";
+        }
+
+        return $this->render('todo/create.html.twig', array(
+          "form" => $form->createView()
+        ));
     }
     /**
      * @Route("/todos/edit/{id}", name="todo_edit")
---
+++ src/AppBundle/Entity/Todos.php
@@ -0,0 +1,190 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Todos
+ *
+ * @ORM\Table(name="todos")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\TodosRepository")
+ */
+class Todos
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="string", length=255)
+     */
+    private $description;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="priority", type="string", length=255)
+     */
+    private $priority;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="due_date", type="string", length=255)
+     */
+    private $dueDate;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="create_date", type="string", length=255)
+     */
+    private $createDate;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Todos
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return Todos
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set priority
+     *
+     * @param string $priority
+     *
+     * @return Todos
+     */
+    public function setPriority($priority)
+    {
+        $this->priority = $priority;
+
+        return $this;
+    }
+
+    /**
+     * Get priority
+     *
+     * @return string
+     */
+    public function getPriority()
+    {
+        return $this->priority;
+    }
+
+    /**
+     * Set dueDate
+     *
+     * @param string $dueDate
+     *
+     * @return Todos
+     */
+    public function setDueDate($dueDate)
+    {
+        $this->dueDate = $dueDate;
+
+        return $this;
+    }
+
+    /**
+     * Get dueDate
+     *
+     * @return string
+     */
+    public function getDueDate()
+    {
+        return $this->dueDate;
+    }
+
+    /**
+     * Set createDate
+     *
+     * @param string $createDate
+     *
+     * @return Todos
+     */
+    public function setCreateDate($createDate)
+    {
+        $this->createDate = $createDate;
+
+        return $this;
+    }
+
+    /**
+     * Get createDate
+     *
+     * @return string
+     */
+    public function getCreateDate()
+    {
+        return $this->createDate;
+    }
+}
+
